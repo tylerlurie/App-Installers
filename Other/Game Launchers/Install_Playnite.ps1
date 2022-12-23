b6552662fadf101092f06bf30740f5e6c2d56584
tylerlurie <118861752+tylerlurie@users.noreply.github.com>
2022-12-23 01:49:05 -0500

    Playnite can now be installed with just one file

--- Other/Game Launchers/Install_Playnite.ps1
+++ Other/Game Launchers/Install_Playnite.ps1
@@ -5,10 +5,13 @@ $dlLink = "https://playnite.link/download/$file"
 $InstallerPath = Join-Path $env:TEMP $file
 (New-Object System.Net.WebClient).DownloadFile($dlLink, $InstallerPath)
 # Playnite does not support silent install, so we will use an AutoHotkey script to automatically direct our input:
+$scriptLink = "https://github.com/tylerlurie/App-Installers/raw/main/Other/Game%20Launchers/Install_Playnite_Inputs.exe"
 $ahkScript = "Install_Playnite_Inputs.exe"
-$ahkScriptPath = "$env:TEMP\$ahkScript" # TODO: Pull this from GitHub so the user doesn't need to have both files on the system
+$ahkScriptPath = "$env:TEMP\$ahkScript"
+(New-Object System.Net.WebClient).DownloadFile($scriptLink, $ahkScriptPath)
 Start-Process $InstallerPath -Verb RunAs
-Start-Process $ahkScript -Wait -Verb RunAs
+Start-Process $ahkScriptPath -Wait -Verb RunAs
+Remove-Item $ahkScriptPath
 While (Test-Path -Path $InstallerPath)
 {
 	Remove-Item $InstallerPath -ErrorAction SilentlyContinue
